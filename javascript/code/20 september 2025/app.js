075809e20349120868eb7ebf434dc8b4d2b79179
InnoSufiyan <innosufiyan@gmail.com>
2025-09-20 20:54:45 +0500

    events

---
+++ javascript/code/20 september 2025/app.js
@@ -0,0 +1,51 @@
+console.log("==>> javascript is running")
+
+// function promptName() {
+//     var name = prompt("What is your name??")
+
+//     document.getElementById('firstName').value = name
+// }
+
+// function signupHandler() {
+//     console.log("==>> kisi ney signup per click kiya")
+//     var firstName = document.getElementById('firstName').value
+//     var lastName = document.getElementById('lastName').value
+//     var email = document.getElementById('email').value
+//     var password = document.getElementById('password').value
+//     var confirmPassword = document.getElementById('confirmPassword').value
+
+//     console.log(firstName,
+//         lastName,
+//         email,
+//         password,
+//         confirmPassword)
+// }
+
+
+// var aboutMe = ` Hi, my name is Muhammad Sufiyan, and i am full stack developer, on the tech of MERN, having good expertise in JavaScript, React, Node blah blah blah`
+
+// document.getElementById('aboutMePara').innerText = aboutMe.slice(0, 35) + "..."
+
+// function seeLess() {
+//     document.getElementById('aboutMePara').innerText = aboutMe.slice(0, 35) + "..." 
+// }
+
+// function seeMore() {
+//     document.getElementById('aboutMePara').innerText = aboutMe  
+// }
+
+// function change() {
+//     document.getElementsByTagName('img')[0].src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAvaQAF3ly-raNnmM1AtPXeNGgEShTGsd9zQ&s"
+// }
+
+// function themeHandler() {
+//     if (document.getElementsByTagName('div')[0].className == "dark") {
+//         document.getElementsByTagName('div')[0].className = "light"
+//     } else {
+//         document.getElementsByTagName('div')[0].className = "dark"
+//     }
+// }
+
+// function fontSizeHandler() {
+//     document.getElementsByTagName('div')[0].className += " fontSize"
+// }
